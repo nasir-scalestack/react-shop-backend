e927042f7b2c8abcd3f975c8b822524803995ff5
Nasir Dowlatkhahi <ndowlat@Nasirs-MacBook-Air.local>
2019-02-25 15:53:13 -0500

    Added new mutations and pagination

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -11,13 +11,27 @@ const Mutations = {
 
     return item;
   },
-    // createDog(parent, args, ctx, info) {
-    //   global.dogs = global.dogs || [];
-    //   // create a dog
-    //   const newDog = { name: args.name };
-    //   global.dogs.push(newDog);
-    //   return newDog;
-    // },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the id from the updates copy
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, 
+    info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id, title }`);
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
   };
   
   module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,9 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
+  item: forwardTo('db'),
   items: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
   // async items(parent, args, ctx, info) {
   //   console.log('Getting Items!!');
   //   const items = await ctx.db.query.items();
