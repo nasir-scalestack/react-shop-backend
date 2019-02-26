5b4a48ec34e097a45a0f739b1c32477d6d8372ce
Nasir <nasir@scalestack.io>
2019-02-26 14:44:46 -0500

    Add user signout

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -76,6 +76,10 @@ const Mutations = {
     });
     return user;
   },
+  async signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye!'}
+  }
   };
   
   module.exports = Mutations;
