4b24aba6a76126171071426de6ddc11757a648eb
Nasir <nasir@scalestack.io>
2019-02-26 16:06:21 -0500

    Send password reset email with nodemailer

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
+const  { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -98,15 +99,15 @@ const Mutations = {
       data: { resetToken, resetTokenExpiry },
     });
     // 3. Email them that reset token
-    // const mailRes = await transport.sendMail({
-    //   from: 'support@reactshop.com',
-    //   to: user.email,
-    //   subject: 'Your Password Reset Token',
-    //   html: makeANiceEmail(`Your Password Reset Token is here!
-    //   \n\n
-    //   <a href="${process.env
-    //     .FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`),
-    // });
+    const mailRes = await transport.sendMail({
+      from: 'support@reactshop.com',
+      to: user.email,
+      subject: 'Your Password Reset Token',
+      html: makeANiceEmail(`Your Password Reset Token is here!
+      \n\n
+      <a href="${process.env
+        .FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`),
+    });
 
     // 4. Return the message
     return { message: 'Thanks!' };
@@ -130,6 +131,8 @@ const Mutations = {
       where: { email: user.email },
       data: {
         password,
+        resetToken: null,
+        resetTokenExpiry: null
       }
     });
 
