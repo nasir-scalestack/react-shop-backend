849494e9ecaf9d8d8fd6c33f68a9b592fdb5a666
Nasir <nasir@scalestack.io>
2019-02-27 12:27:38 -0500

    Fix permissions not updating bug

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
+const { hasPermission } = require('../utils');
 const  { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
@@ -26,6 +27,37 @@ const Mutations = {
     );
 
     return item;
+  },
+  async updatePermissions(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    // 2. Query the current user
+    const currentUser = await ctx.db.query.user(
+      {
+        where: {
+          id: ctx.request.userId,
+        },
+      },
+      info
+    );
+    // 3. Check if they have permissions to do this
+    hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE']);
+    // 4. Update the permissions
+    return ctx.db.mutation.updateUser(
+      {
+        data: {
+          permissions: {
+            set: args.permissions,
+          },
+        },
+        where: {
+          id: args.userId,
+        },
+      },
+      info
+    );
   },
   updateItem(parent, args, ctx, info) {
     // first take a copy of the updates
